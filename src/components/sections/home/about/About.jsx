ade7796a56182dca7a6b8aef1b5b495cbb0b8f59
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-10-10 21:25:01 +0100

    Add about comp, absolute hero content center

--- src/components/sections/home/about/About.jsx
+++ src/components/sections/home/about/About.jsx
@@ -13,22 +13,36 @@ export const About = () => {
             velit perspiciatis harum. Reprehenderit voluptates ab iusto, neque
             vero similique ea!
           </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
+            error dicta tempore sapiente vero modi magni, veritatis nemo ex,
+            velit perspiciatis harum. Reprehenderit voluptates ab iusto, neque
+            vero similique ea!
+          </p>
         </div>
         <div className="about__container-right">
-          <div className="image-comp image-comp--1">
-            <img
-              src="img/rolands-varsbergs-miKmVyq3qhE-unsplash.jpg"
-              alt="Photo 1"
-            />
-          </div>
-          <div className="image-comp image-comp--2">
-            <img src="img/pexels-jaime-reimer-2749500.jpg" alt="Photo 2" />
-          </div>
-          <div className="image-comp image-comp--3">
-            <img
-              src="img/noah-buscher-x8ZStukS2PM-unsplash.jpg"
-              alt="Photo 3"
-            />
+          <div className="image-comp">
+            <div className="image-comp__container image-comp__container--1">
+              <img
+                className="image-comp__image"
+                src="img/rolands-varsbergs-miKmVyq3qhE-unsplash.jpg"
+                alt="Photo 1"
+              />
+            </div>
+            <div className="image-comp__container image-comp__container--2">
+              <img
+                className="image-comp__image"
+                src="img/pexels-jaime-reimer-2749500.jpg"
+                alt="Photo 2"
+              />
+            </div>
+            <div className="image-comp__container image-comp__container--3">
+              <img
+                className="image-comp__image"
+                src="img/noah-buscher-x8ZStukS2PM-unsplash.jpg"
+                alt="Photo 3"
+              />
+            </div>
           </div>
         </div>
       </div>
