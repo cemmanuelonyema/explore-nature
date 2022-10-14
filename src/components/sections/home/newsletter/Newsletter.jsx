11aeec374568fe2572d0b62cb9dcac7eda51cc04
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-10-14 23:35:01 +0100

    Replace contact section with newsletter section

---
+++ src/components/sections/home/newsletter/Newsletter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./newsletter.scss";
+
+export const Newsletter = () => {
+  return (
+    <section className="newsletter" id="newsletter">
+      <div className="newsletter__container">
+        <div className="newsletter__content">
+          <div className="newsletter__form">
+            <h2 class="newsletter__heading">
+              Want To Be The First To Know About Our New Tours Destination, And
+              Discounts.
+            </h2>
+            <form className="form">
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Email Address"
+              />
+              <button>Subscribe</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import { TourPlans } from "../../components/sections/home/tour/TourPlans";
 import { Hero } from "../../components/sections/home/hero/Hero";
 import { Nav } from "../../components/layout/nav/Nav";
 import { About } from "../../components/sections/home/about/About";
-import { Contact } from "../../components/sections/home/contact/Contact";
+import { Newsletter } from "../../components/sections/home/newsletter/Newsletter";
 
 export const Home = () => {
   return (
@@ -16,7 +16,7 @@ export const Home = () => {
       <About />
       <Features />
       <TourPlans />
-      <Contact />
+      <Newsletter />
       <Footer />
     </Fragment>
   );
