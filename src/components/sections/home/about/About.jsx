1520d828fee62bdd6035c61d7f14c74939b5011c
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-11-19 18:09:25 +0100

    modify about section layout

--- src/components/sections/home/about/About.jsx
+++ src/components/sections/home/about/About.jsx
@@ -21,28 +21,12 @@ export const About = () => {
           </p>
         </div>
         <div className="about__container-right">
-          <div className="image-comp">
-            <div className="image-comp__container image-comp__container--1">
-              <img
-                className="image-comp__image"
-                src="img/rolands-varsbergs-miKmVyq3qhE-unsplash.jpg"
-                alt="Photo 1"
-              />
-            </div>
-            <div className="image-comp__container image-comp__container--2">
-              <img
-                className="image-comp__image"
-                src="img/pexels-jaime-reimer-2749500.jpg"
-                alt="Photo 2"
-              />
-            </div>
-            <div className="image-comp__container image-comp__container--3">
-              <img
-                className="image-comp__image"
-                src="img/noah-buscher-x8ZStukS2PM-unsplash.jpg"
-                alt="Photo 3"
-              />
-            </div>
+          <div className="image-box">
+            <img
+              className="image-comp__image"
+              src="img/pexels-jaime-reimer-2749500.jpg"
+              alt="Photo 2"
+            />
           </div>
         </div>
       </div>
